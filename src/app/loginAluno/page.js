bba1dddc065de6868137c61d5be1141cc16c208f
oRdv <tamiressenaisp@gmail.com>
2024-09-16 22:30:17 -0300

    feat: finished the login page

--- src/app/loginAluno/page.js
+++ src/app/loginAluno/page.js
@@ -9,24 +9,32 @@ function Login() {
           <h1>ProVest</h1>
         </div>
         <div className={styles.illustration}>
-          {/* Place your photo here */}
+
         </div>
       </div>
+
+
       <div className={styles['right-side']}>
+
         <div className={styles.welcome}>
           <h1>BEM VINDO ALUNO!</h1>
         </div>
+
+
         <div className={styles['login-form']}>
           <h1>Login</h1>
           <form>
+
             <div className={styles['form-group']}>
               <label htmlFor="email">E-mail</label>
               <input type="email" id="email" placeholder="Seu e-mail" />
             </div>
+
             <div className={styles['form-group']}>
               <label htmlFor="password">Senha</label>
               <input type="password" id="password" placeholder="Sua senha" />
             </div>
+
             <button type="submit" className={styles['btn-login']}>
               LOGIN
             </button>
@@ -34,8 +42,11 @@ function Login() {
         </div>
         <div className={styles['forgot-password']}>
           <a href="#">Esqueceu a senha?</a>
-          <input type="checkbox" id="remember-me" />
-          <label htmlFor="remember-me">Lembrar de mim</label>
+          <label className={styles['custom-checkbox']}>
+            <input type="checkbox" id="remember-me" />
+            <span className={styles['checkmark']}></span>
+            <span className={styles['label-text']}>Lembrar de mim</span>
+          </label>
         </div>
         <div className={styles['create-account']}>
           <a href="#">Não possui cadastro?</a>
@@ -45,6 +56,5 @@ function Login() {
     </div>
   );
 }
-
 
 export default Login;
