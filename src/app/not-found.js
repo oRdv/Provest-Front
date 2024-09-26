99a2f642fc854e69562f70c8c5189e02bed6d555
23114984 <lima.nyck01@gmail.com>
2024-09-26 17:12:45 -0300

    fix: change img

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,29 +1,31 @@
 import React from "react";
-import styles from './not-found.css'; 
+import styles from './not-found.module.css'; 
 import Image from 'next/image';
 import Link from 'next/link';
 
-// function notFound() {
-//         return (
-//           <div className={styles.container}>
-//             <Image
-//               src="/path/to/your/image.jpg" 
-//               width={300} 
-//               height={300} 
-//               className={styles.image}
-//             />
-//             <p className={styles.text}>PÁGINA EM CONSTRUÇÃO!
-//             EXPLORE OUTRAS ÁREAS DO NOSSO APLICATIVO.</p>
-//             <div className={styles.buttons}>
-//               <Link href="/" legacyBehavior>
-//                 <span className={styles.button}>HOME</span>
-//               </Link>
-//               <Link href="/profile" legacyBehavior>
-//                 <span className={styles.button}>MEU CRONOGRAMA</span>
-//               </Link>
-//             </div>
-//           </div>
-//         );
-//       }
+function NotFound() { 
+    return (
+        <div className={styles.container}>
+          <h1 className={styles.text}>PÁGINA EM CONSTRUÇÃO!</h1>
+          <p className={styles.text}>EXPLORE OUTRAS ÁREAS DO</p>
+          <p className={styles.text}> NOSSO APLICATIVO.</p>
+          <div className={styles.buttons}>
+            <Image
+                src="/images/confuso.png" // colocar a imagem do menino confuso
+                width={300}
+                height={300}
+                className={styles.image}
+            />
+            
+                <Link href="/" legacyBehavior>
+                    <span className={styles.button}>HOME</span>
+                </Link>
+                <Link href="/profile" legacyBehavior>
+                    <span className={styles.button}>MEU CRONOGRAMA</span>
+                </Link>
+            </div>
+        </div>
+    );
+}
 
-// export default notFound;
+export default NotFound;
