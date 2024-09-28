421d5a9b7c877cc0977fc51f7eb3794c7a7ebe8a
oRdv <tamiressenaisp@gmail.com>
2024-09-28 12:28:26 -0300

    fix: finish page

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,22 +1,34 @@
 import React from "react";
-import styles from './not-found.module.css'; 
+import styles from './not-found.module.css';
 import Image from 'next/image';
 import Link from 'next/link';
 
-function NotFound() { 
+function NotFound() {
     return (
-        <div className={styles.container}>
-          <h1 className={styles.text}>PÁGINA EM CONSTRUÇÃO!</h1>
-          <p className={styles.text}>EXPLORE OUTRAS ÁREAS DO</p>
-          <p className={styles.text}> NOSSO APLICATIVO.</p>
-          <div className={styles.buttons}>
+        
+        <div className={styles.pro}>
+
             <Image
-                src="/images/confuso.png" // colocar a imagem do menino confuso
-                width={300}
-                height={300}
-                className={styles.image}
+                src="/img/logo-verde.png"
+                width={90}
+                height={87}
+                alt="Desenho de uma mulher pensando"
             />
             
+        <div className={styles.container}>
+                <h1 className={styles.text}>PÁGINA EM CONSTRUÇÃO!</h1>
+                <p className={styles.text}>EXPLORE OUTRAS ÁREAS DO</p>
+                <p className={styles.text}> NOSSO APLICATIVO.</p>
+                <div className={styles.buttons}>
+
+                    <Image
+                        src="/img/confuso.png"
+                        width={360}
+                        height={300}
+                        alt="Desenho de uma mulher pensando"
+                    />
+                </div>
+
                 <Link href="/" legacyBehavior>
                     <span className={styles.button}>HOME</span>
                 </Link>
