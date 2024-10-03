115fc5c493136a09664e9379973eccefab70e0c3
Tamires Fernandes <124786323+oRdv@users.noreply.github.com>
2024-10-03 17:00:23 -0300

    fix: update password

--- src/app/recuperarSenha/page.js
+++ src/app/recuperarSenha/page.js
@@ -1,10 +1,62 @@
+'use client'
+import React, { useState } from 'react';
 import Image from 'next/image';
 import styles from './page.module.css';
 
 function Recuperar() {
+  const [formData, setFormData] = useState({
+    email: '',
+    confirmEmail: '',
+    senha: '',
+    confirmSenha: ''
+  });
+  const [error, setError] = useState('');
+  const [success, setSuccess] = useState('');
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+
+    try {
+      const userResponse = await fetch('https://jengt-provest-backend.onrender.com/v1/jengt_provest/alunos');
+      const usersData = await userResponse.json();
+
+      const user = usersData.alunos.find((aluno) => aluno.email === formData.email);
+
+      if (!user) {
+        setError('Usuário não encontrado.');
+        return;
+      }
+
+      const updateResponse = await fetch(`https://jengt-provest-backend.onrender.com/v1/jengt_provest/aluno/senha/${user.id}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          senha: formData.senha,
+        }),
+      });
+
+      const updateData = await updateResponse.json();
+
+      if (updateResponse.ok) {
+        setSuccess('Senha atualizada com sucesso!');
+      } else {
+        setError(updateData.message || 'Erro ao atualizar a senha.');
+      }
+    } catch (err) {
+      setError('Erro na solicitação.');
+    }
+  };
+
   return (
     <div className={styles.container}>
-
       <div className={styles['left-side']}>
         <div className={styles.provest}>
           <h1>ProVest</h1>
@@ -25,26 +77,33 @@ function Recuperar() {
         </div>
 
         <div className={styles['login-form']}>
-          <form>
+          <form onSubmit={handleSubmit}>
             <div className={styles['form-group']}>
               <label htmlFor="email">E-mail</label>
-              <input type="email" id="email" />
+              <input
+                type="email"
+                id="email"
+                name="email"
+                value={formData.email}
+                onChange={handleInputChange}
+                required
+              />
             </div>
 
             <div className={styles['form-group']}>
-              <label htmlFor="email">Confirme seu E-mail</label>
-              <input type="email" id="email" />
+              <label htmlFor="senha">Senha</label>
+              <input
+                type="password"
+                id="senha"
+                name="senha"
+                value={formData.senha}
+                onChange={handleInputChange}
+                required
+              />
             </div>
 
-            <div className={styles['form-group']}>
-              <label htmlFor="email">Senha</label>
-              <input type="email" id="email" />
-            </div>
-
-            <div className={styles['form-group']}>
-              <label htmlFor="password">Confirme sua enha</label>
-              <input type="password" id="password" />
-            </div>
+            {error && <p className={styles.error}>{error}</p>}
+            {success && <p className={styles.success}>{success}</p>}
 
             <div className={styles['button-container']}>
               <button type="submit" className={styles['btn-login']}>
@@ -53,8 +112,6 @@ function Recuperar() {
             </div>
           </form>
         </div>
-
-        <hr className="horizontal-line" />
       </div>
     </div>
   );
