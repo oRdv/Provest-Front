35db33ec57e297cf0d231bb1e3985a07f44f8aea
Tamires Fernandes <124786323+oRdv@users.noreply.github.com>
2024-10-10 12:22:09 -0300

    fix: done the screen of essay

--- src/app/escreverRedacao/page.js
+++ src/app/escreverRedacao/page.js
@@ -1,46 +1,70 @@
 "use client";
 import styles from './page.modules.css';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import Link from 'next/link';
 
 function EscreverRedacao() {
-  const [title, setTitle] = useState(''); 
-  const [text, setText] = useState('');   
+  const [text, setText] = useState('');
+  const [lineCount, setLineCount] = useState(0);
+  const maxLines = 30;
+  const maxCharsPerLine = 65;
 
-  const handleTitleChange = (e) => {
-    setTitle(e.target.value);
+  const handleChange = (e) => {
+    let inputText = e.target.value;
+    let lines = inputText.split('\n');
+
+    let currentLineCount = 0;
+    lines.forEach(line => {
+      currentLineCount += Math.ceil(line.length / maxCharsPerLine);
+    });
+
+    if (currentLineCount <= maxLines) {
+      setText(inputText);
+      setLineCount(currentLineCount);
+    }
   };
 
-  const handleTextChange = (e) => {
-    setText(e.target.value);
-  };
+  const [selectedTheme, setSelectedTheme] = useState('');
 
+  useEffect(() => {
+    const theme = localStorage.getItem('selectedTheme');
+    if (theme) {
+      setSelectedTheme(theme);
+      console.log(selectedTheme);
+
+    }
+  }, []);
   return (
     <div className="container">
       <div className="header">
         <h1>Vamos escrever!</h1>
       </div>
 
-      <div className="theme1">
-        <p>Tema escolhido:</p>
-        </div>
+      <div className="theme-text">
 
-        <div className="theme-text">
-        <div className="text">
-          <b>Desafios para o enfrentamento da invisibilidade do trabalho de cuidado realizado pela mulher no Brasil.</b>
-        </div>
-        </div>
+        <div className="theme1">
+          <p>Tema escolhido:</p>
 
-      <div className="redacao-container">
+          <div className="text">
+            <b>{selectedTheme || "Nenhum tema selecionado"}</b>
+          </div>
+        </div>
+      </div>
 
-      <div className="text-area">
-        <p>Escreva seu texto aqui</p>
+      <div className="redacao_container">
+        <input type="text" className="titulo_redacao" placeholder="Título da Redação" />
+
         <textarea
-          value={text}
-          onChange={handleTextChange}
-          className="text-area-field"
-        />
+          className="textarea_redacao"
+          rows="30"
+          cols="90"
+          placeholder="Escreva sua redação aqui..." value={text} onChange={handleChange}
+        ></textarea>
+
+        <p className="line-counter">Linhas: {lineCount}/{maxLines}</p>
       </div>
-      </div>
+
+      <Link href="../escreverRedacao" className="buttonWrite">Enviar para correção</Link>
     </div>
   );
 }
