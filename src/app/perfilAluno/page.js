f3a85b5137579ac02cd2eba0ef99081200cee669
Tamires Fernandes <124786323+oRdv@users.noreply.github.com>
2024-10-17 17:11:07 -0300

    fix: modal done

--- src/app/perfilAluno/page.js
+++ src/app/perfilAluno/page.js
@@ -6,7 +6,6 @@ import AvatarSelector from '@/components/ui/AvatarSelector';
 
 const ProfilePage = () => {
   const [profile, setProfile] = useState({
-    photo: '',
     name: '',
     phone: '',
     email: '',
@@ -37,67 +36,69 @@ const ProfilePage = () => {
 
   return (
     <div className={styles["profile-page"]}>
-      <div className={styles["top-banner"]} />
       <div className={styles["header"]}>
-        <h1>NYCOLLE LIMA</h1>
-        <div className={styles["avatar-preview"]}>
-        {avatar ? (
-          <img src={avatar} alt="Avatar" />
-        ) : (
-          'Escolha um avatar'
-        )}
+        <div className={styles["avatar-preview"]} onClick={() => setIsModalOpen(true)}>
+          {avatar ? (
+            <img src={avatar} alt="Avatar" />
+          ) : (
+            <div></div>
+          )}
+        </div>
+        <h1 className={styles["profile-name"]}>NYCOLLE LIMA</h1>
       </div>
-      <button onClick={() => setIsModalOpen(true)}>Escolher Avatar</button>
 
-      <Modal isOpen={isModalOpen} onRequestClose={() => setIsModalOpen(false)} className={'tamara'} contentLabel="Select Avatar">
+      <Modal
+        isOpen={isModalOpen}
+        onRequestClose={() => setIsModalOpen(false)}
+        className={styles.modal}
+        contentLabel="Select Avatar"
+      >
         <AvatarSelector onSelect={handleAvatarSelect} />
       </Modal>
-    </div>
 
       <form className={styles["profile-form"]} onSubmit={handleSubmit}>
         <div className={styles["form-group"]}>
-          <label htmlFor="name">Nome</label>
           <input
             type="text"
             id="name"
             name="name"
+            placeholder="Nome"
             value={profile.name}
             onChange={handleChange}
           />
         </div>
         <div className={styles["form-group"]}>
-          <label htmlFor="phone">Telefone</label>
           <input
             type="tel"
             id="phone"
             name="phone"
+            placeholder="Telefone"
             value={profile.phone}
             onChange={handleChange}
           />
         </div>
         <div className={styles["form-group"]}>
-          <label htmlFor="email">Email</label>
           <input
             type="email"
             id="email"
             name="email"
+            placeholder="Email"
             value={profile.email}
             onChange={handleChange}
           />
         </div>
         <div className={styles["form-group"]}>
-          <label htmlFor="password">Senha</label>
           <input
             type="password"
             id="password"
             name="password"
+            placeholder="Mudar senha"
             value={profile.password}
             onChange={handleChange}
           />
         </div>
+        <button type="submit" className={styles["save-button"]}>SALVAR</button>
       </form>
-      <span className={styles.button}>SALVAR</span>
-
     </div>
   );
 };
