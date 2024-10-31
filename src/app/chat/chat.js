c9e78e04dfee78ef9f976b2e5f1c39aa7e6aba65
TAMIRES SILVA FERNANDES <23111766@dsn127m1242376.cfp127aluno.local>
2024-10-31 09:14:51 -0300

    feat: initialize chat

---
+++ src/app/chat/chat.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBPR-s9gjxwaFogDx7CEt7_Zwjrlayn8Io",
+  authDomain: "chat-provest.firebaseapp.com",
+  databaseURL: "https://chat-provest-default-rtdb.firebaseio.com",
+  projectId: "chat-provest",
+  storageBucket: "chat-provest.appspot.com",
+  messagingSenderId: "552821901016",
+  appId: "1:552821901016:web:e9cccbbd59922bc6cf39ad"
+};
+
+
+const app = initializeApp(firebaseConfig);
